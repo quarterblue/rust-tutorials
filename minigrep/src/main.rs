3ae783a7911bb7a18523d47a9a29355085f12a18
quarterblue <quarterslate@gmail.com>
2021-03-18 12:17:02 -0700

    Refactor to improve error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,47 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for {}", query);
-    println!("Searching for {}", filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(filename)
+    println!("Searching for {}", config.query);
+    println!("Searching for {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
 }
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config {query, filename}
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str>{
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config {query, filename})
+    }
+}
+
+
